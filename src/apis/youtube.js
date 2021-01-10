8a4eb480fedde0fb9aeb994b1bb5cacc78667d29
Jaakko Rajala <jaakko.rajala@tuni.fi>
2021-01-11 01:26:56 +0200

    Minor fixes.

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const KEY = 'AIzaSyCVq0kap_fdJNgx-EwPe7iIepbiO9EnfIY';
+const KEY = process.env.YOUTUBE_API_KEY;
 
 export default axios.create({
 	baseURL: 'https://www.googleapis.com/youtube/v3',
